9bd802bade897f73967dbda8bed2fd20be190ac5
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2021-06-20 00:56:55 +0530

    Add files via upload

---
+++ K Inverse Pairs Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int kInversePairs(int n, int k) {
+       int[][]dp = new int[n+1][k+1];
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j <= k; j++){
+                if(j == 0) dp[i][j] = 1;    //when j = 0(k = 0) then sorted array is the only solution
+                else{
+                    for(int p = 0; p <= Math.min(j,i-1); p++)
+                        dp[i][j] = (dp[i][j] + dp[i-1][j-p]) % 1000000007;
+                }
+            }
+        }
+        return dp[n][k];
+    }
+}
